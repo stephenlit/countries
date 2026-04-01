5a0ff931efb12576ecfab2ba9da2016c95e6d834
stephenlit <stevish45@icloud.com>
2026-04-01 18:00:37 -0300

    initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useCountries } from "@/composables/useCountries";
+
+const { countries, loading, error } = useCountries();
+</script>
+
+<template>
+  <div>
+    <p v-if="loading">Loading...</p>
+    <p v-if="error">{{ error }}</p>
+    <pre>{{ countries[0] }}</pre>
+  </div>
+</template>
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,8 @@
+<template>
+  <header class="bg-blue-900 text-white p-4 h-20 flex items-center font-bold tracking-wide">
+    <h1>Where in the world?</h1>
+  </header>
+  <main class="p-4 bg-blue-950">
+    <slot />
+  </main>
+</template>
