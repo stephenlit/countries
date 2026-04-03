07be1cd6757312a14300fe9f8622c9109ab07099
stephenlit <stevish45@gmail.com>
2026-04-03 06:39:16 -0300

    changes to CountryView

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,8 +1,8 @@
 <template>
-  <header class="bg-blue-900 text-white p-4 h-20 flex items-center font-bold tracking-wide">
+  <header class="px-40 bg-blue-900 text-white p-4 h-20 flex items-center font-bold tracking-wide">
     <h1>Where in the world?</h1>
   </header>
-  <main class="p-4 bg-blue-950">
+  <main class="p-4h-screen">
     <slot />
   </main>
 </template>
--- src/views/CountryView.vue
+++ src/views/CountryView.vue
@@ -14,34 +14,30 @@ onMounted(() => {
 </script>
 
 <template>
-  <main class="mx-auto w-full max-w-3xl px-6 py-10 h-screen">
-    <RouterLink
-      to="/"
-      class="inline-block rounded-md bg-blue-900 px-4 py-2 text-sm font-semibold text-grey-50 shadow-md transition hover:bg-blue-950"
-    >
-      Back
-    </RouterLink>
+  <div v-if="loading" class="mt-8 text-grey-50">Loading...</div>
+  <p v-else-if="error" class="mt-8 text-base text-red-400">{{ error }}</p>
 
-    <div v-if="loading" class="mt-8 text-grey-50">Loading...</div>
-    <p v-else-if="error" class="mt-8 text-base text-red-400">{{ error }}</p>
+  <section v-else-if="country[0]" class="h-full px-40 mt-8 rounded-lg text-grey-50">
+    <div class="mb-6">
+      <RouterLink
+        to="/"
+        class="inline-block rounded-md bg-blue-900 px-4 py-2 text-sm font-semibold text-grey-50 shadow-md transition hover:bg-blue-950"
+      >
+        Back
+      </RouterLink>
+    </div>
+    <div class="flex flex-wrap">
+      <img :src="country[0].flag" :alt="`${country[0].name} flag`" class="w-1/2 shrink-0" />
 
-    <section v-else-if="country[0]" class="mt-8 rounded-lg p-6 text-grey-50 flex gap-10">
-      <div class="w-100 shrink-0">
-        <img
-          :src="country[0].flag"
-          :alt="`${country[0].name} flag`"
-          class="h-52 w-full object-contain"
-        />
-      </div>
-      <div>
+      <div class="w-1/2 ml-6">
         <h1 class="mt-6 text-lg text-white font-bold">{{ country[0].name }}</h1>
         <h4 class="mt-1 text-base">Native Name: {{ country[0].nativeName }}</h4>
         <p class="mt-3 text-base">Population: {{ country[0].population.toLocaleString() }}</p>
         <p class="text-base">Region: {{ country[0].region }}</p>
         <p class="text-base">Capital: {{ country[0].capital || "N/A" }}</p>
       </div>
-    </section>
+    </div>
+  </section>
 
-    <p v-else class="mt-8 text-base">Country not found.</p>
-  </main>
+  <p v-else class="mt-8 text-base">Country not found.</p>
 </template>
